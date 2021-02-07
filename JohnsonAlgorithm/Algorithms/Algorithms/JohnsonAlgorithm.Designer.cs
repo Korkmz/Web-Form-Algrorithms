fec7684c54856d8678faa9e776644f5d61c1e1e6
unknown <hatice_hh-@hotmail.com>
2021-02-07 09:31:52 +0300

    first

---
+++ JohnsonAlgorithm/Algorithms/Algorithms/JohnsonAlgorithm.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace Algorithms
+{
+    partial class JohnsonAlgorithm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            this.JohnsonCacl = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(164, 16);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(171, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "2. Machine";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(164, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(171, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "1. Machine";
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(379, 16);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(70, 40);
+            this.Clear.TabIndex = 13;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(455, 16);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(84, 40);
+            this.Add.TabIndex = 12;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // JohnsonCacl
+            // 
+            this.JohnsonCacl.Location = new System.Drawing.Point(379, 79);
+            this.JohnsonCacl.Name = "JohnsonCacl";
+            this.JohnsonCacl.Size = new System.Drawing.Size(160, 45);
+            this.JohnsonCacl.TabIndex = 11;
+            this.JohnsonCacl.Text = "Johnson Algrithm";
+            this.JohnsonCacl.UseVisualStyleBackColor = true;
+            this.JohnsonCacl.Click += new System.EventHandler(this.JohnsonCal_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(376, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(376, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Sonuc :";
+            // 
+            // JohnsonAlgorithm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.JohnsonCacl);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "JohnsonAlgorithm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button JohnsonCacl;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ JohnsonAlgorithm/Algorithms/Algorithms/JohnsonAlgorithm.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Algorithms
+{
+    public partial class JohnsonAlgorithm : Form
+    {
+        public JohnsonAlgorithm()
+        {
+            InitializeComponent();
+        }
+
+        private void JohnsonCal_Click(object sender, EventArgs e)
+        {
+            var machines = GetList();  //Ekranda Girilen İnput lar iş parcacikları liste olar döndürür.
+
+            List<int> result = JohnsonAlgrithm(machines); //Johnson alg. hesaplar sonucu döndürür.
+
+            Write(result);  //sonucu ekrana yazdırırç
+        }
+
+
+        public List<List<int>> GetList() //Ekranda Girilen İnput lar iş parcacikları listeye ekler
+        {
+            List<List<int>> machines = new List<List<int>>();
+
+            foreach (TextBox tb in this.Controls.OfType<TextBox>()) //Ekrandaki Tüm inputları for döngüsüne koyar
+            {
+                if (!String.IsNullOrEmpty(tb.Text))
+                {
+                    var list1 = tb.Text.Split(',');//input lardaki virgüllerle ayrılımış iş parcacıklarını alır.
+                    List<int> m1 = new List<int>();
+
+                    foreach (var item in list1)
+                    {
+                        m1.Add(int.Parse(item)); //Makineye ekler
+                    }
+                    machines.Add(m1);
+                }
+            }
+            return machines;//Alınan makineyi geri döndürür.
+        }
+
+
+        public List<int> JohnsonAlgrithm(List<List<int>> machines)
+        {
+            #region İki Makineye indirgeme yapıyor
+            List<int> AA = new List<int>();
+            for (var y = 0; y < machines[0].Count; y++)
+            {
+                var total = 0;
+                for (var t = 0; t < machines.Count() - 1; t++)
+                {
+                    total += machines[t][y];   //machines[0][0]  machines[1][0]
+                }
+                AA.Add(total);
+            }
+            List<int> BB = new List<int>();
+            for (var t = 0; t < machines[0].Count(); t++)
+            {
+                var total = 0;
+                for (var y = 1; y < machines.Count; y++)
+                {
+                    total += machines[y][t];
+                }
+                BB.Add(total);
+
+            }
+            #endregion
+
+
+            var listCount = AA.Count;
+
+            List<int> L1 = new List<int>();
+            List<int> L2 = new List<int>();
+
+
+            List<int> m11 = new List<int>(AA);
+            List<int> m22 = new List<int>(BB);
+
+            for (var x = 0; x < listCount; x++)
+            {
+                var newList2 = L1.Concat(L2);
+                var makine1 = m11.Min(); //Makine 1 deki en küçük değer
+                var makine2 = m22.Min(); //Makine 2 deki en küçük değer
+                if (makine1 < makine2)   
+                {
+                    var indexNo = m11.IndexOf(makine1);
+                    var pNo = AA.IndexOf(makine1);
+                    m11.RemoveAt(indexNo);
+                    m22.RemoveAt(indexNo);
+                    #region Eger Diziye eklenmişse bu sayı 
+                    var sayac = 0;
+                    foreach (var item in newList2)
+                    {
+                        if (item == (pNo + 1))
+                        {
+                            sayac++;
+                        }
+                    }
+                    if (sayac != 0) pNo = AA.IndexOf(makine1, (pNo + 1));
+                    #endregion
+                    L1.Add((pNo + 1)); //makine1
+                }
+                else
+                {
+                    var indexNo = m22.IndexOf(makine2);
+                    var pNo = BB.IndexOf(makine2);
+                    m11.RemoveAt(indexNo);
+                    m22.RemoveAt(indexNo);
+                    #region Eger Diziye eklenmişse bu sayı 
+                    var sayac = 0;
+                    foreach (var item in newList2)
+                    {
+                        if (item == (pNo + 1))
+                        {
+                            sayac++;
+                        }
+                    }
+                    if (sayac != 0) pNo = BB.IndexOf(makine2, (pNo + 1));
+                    #endregion
+                    L2.Insert(0, (pNo + 1));//makine2
+
+                }
+            }
+
+            L1.AddRange(L2); //L1 makinesinde birleştiriyor
+            return L1;
+
+
+        }
+
+
+        public void Write(List<int> result)
+        {
+            var sonuc = "";
+            foreach (var item in result)
+            {
+                sonuc += "J" + item + " , ";
+            }
+
+            label4.Text = sonuc; //Sonucu Ekrana Yazdırır.
+        }
+     
+        
+        private void Add_Click(object sender, EventArgs e) //Satır ekleme yapıyor(input ekliyor)
+        {
+            var txtRun = new TextBox();
+            int count = this.Controls.OfType<TextBox>().ToList().Count; //Ekranda textbox sayısını buluyor
+            txtRun.Location = new System.Drawing.Point(122, 40 * count); //Ekrana yerleştirieceğei konumu ayaralıyor
+            txtRun.Size = new System.Drawing.Size(128, 22);    //Textbox boyutunu ayarlıyor 
+
+
+            var lblRun = new Label();
+            lblRun.Text = (count + 1) + ". Machine";
+            lblRun.Name = "machine";
+            lblRun.Location = new System.Drawing.Point(50, 40 * count);
+
+            this.Controls.Add(txtRun);
+            this.Controls.Add(lblRun);
+        }
+
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            foreach (TextBox tb in this.Controls.OfType<TextBox>())
+            {
+                if (tb.Name != "textBox2" & tb.Name != "textBox1") //İlk 2 makine (input) silinemez!
+                {
+                    tb.Clear();
+                    this.Controls.Remove(tb);
+                    tb.Dispose();
+                }
+
+            }
+            textBox1.Text = "";
+            textBox2.Text = "";
+            foreach (Label tb in this.Controls.OfType<Label>())
+            {
+                if (tb.Name == "machine")
+                {
+                    this.Controls.Remove(tb);
+                    tb.Dispose();
+                }
+
+            }
+        }
+
+
+
+    }
+}
